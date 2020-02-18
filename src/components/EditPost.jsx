8505bf184f25f3bb6e71672b280320d86c5e60c1
Afasha Isakiye <afashaisakiye@gmail.com>
2020-02-18 18:22:32 +0300

    Now edit post ui is working

--- src/components/EditPost.jsx
+++ src/components/EditPost.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React ,{useState} from 'react'
 
 export const EditPost = ({index}) => {
+    const posts=JSON.parse(window.localStorage.getItem("posts"))[index]
+    const [newPost, setPost] = useState(posts);
     return (
         <div id="edit-post-form" className="container edit-post visible overlay" >
             <span onClick={onclose} id="edit-close-btn" className="cancel-button pull-right">
@@ -10,9 +12,9 @@ export const EditPost = ({index}) => {
             <h2>Edit your post</h2>
             <form className="form-group" id="edit-blog-form" method="post">
             <h3>
-                <input name="title"  required placeholder="Title" className="form-control" type="text" id="edit-title" autoFocus="" /> </h3> <br/>
+                <input value={newPost.title} name="title" required placeholder="Title" className="form-control" type="text" id="edit-title" autoFocus="" /> </h3> <br/>
                 <p>
-                    <textarea name="body"  required placeholder="Type your body here." className="form-control" id="edit-body" rows="3"></textarea></p>
+                    <textarea value={newPost.body} name="body"  required placeholder="Type your body here." className="form-control" id="edit-body" rows="3"></textarea></p>
         <button className="form-control" id="edit-button" type="submit">Save Post</button>
         </form>
         </div>
